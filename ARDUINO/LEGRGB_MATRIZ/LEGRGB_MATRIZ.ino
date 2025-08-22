01ef039b3403019fdc79b9735aa8c3d3b4f8d87f
_Malagutt1_ <kauamarcosmalagutti2003@gmail.com>
2025-08-22 17:24:47 -0300

    led RGB
    muda de cor atraves do potenciometro

--- ARDUINO/LEGRGB_MATRIZ/LEGRGB_MATRIZ.ino
+++ ARDUINO/LEGRGB_MATRIZ/LEGRGB_MATRIZ.ino
@@ -1,15 +1,3 @@
-
-/*Montar um circuito com led RGB + potenciômetro para selecionar uma cor entre as seis cores disponíveis na MatrizRGB.
-
-  Ao pressionar o PushButtom1 pegar o valor do potenciômetro(0-5) e selecionar a cor definida na Matriz. 
-  O LedRGB deverá acender na respectiva cor.
-
-  O PushButtom2 serve para mostrar quantas vezes cada cor foi selecionada bem como seu percentual 
-  em relação ao total de cores escolhidas.
-
-  Ao final, salve o código em um arquivo juntamente com uma foto do projeto, 
-  coloque os dois arquivos em uma pasta zipada e envie como resposta desta atividade.
-*/
 int pinos[] = {11, 10, 9}; //R | G | B
 int PushButtom1 = 2;
 int pushMostraMatriz = 4;
@@ -19,45 +7,55 @@ int matrizRGB[6][4] = { // red | green | blue | contador
   {0, 0, 255, 0},//blue
   {255, 255, 0, 0}, // AMARELO
   {150, 0, 255, 0}, //MAGENTA
-  {128, 0, 128, 0}  // ROSDA
+  {100, 0, 128, 0}  // ROSA
 };
 
 void LeituraPotenciometro() {
   Serial.println("##### LEITURA DO POTENCIOMETRO #####");
   int linha = map(analogRead(A0), 0, 1023, 1, 6);
 
-  if (linha == 1) {
+  if (linha == 1){
     Serial.println("##### COR VERMELHA NO LED! #####");
-    Serial.println("");
-    matrizRGB[linha][3] = matrizRGB[linha][3] + 1;
+    matrizRGB[linha][3]++;
+    analogWrite(pinos[0], matrizRGB[linha][0]);
+    analogWrite(pinos[1], matrizRGB[linha][1]);
+    analogWrite(pinos[2], matrizRGB[linha][2]);
   }
-  else if (linha == 2) {
+  else if (linha == 2){
     Serial.println("##### COR VERDE NO LED! #####");
-    Serial.println("");
-    matrizRGB[linha][3] = matrizRGB[linha][3] + 1;
+    matrizRGB[linha][3]++;
+    analogWrite(pinos[0], matrizRGB[linha][0]);
+    analogWrite(pinos[1], matrizRGB[linha][1]);
+    analogWrite(pinos[2], matrizRGB[linha][2]);
   }
-  else if (linha == 3) {
+  else if (linha == 3){
     Serial.println("##### COR AZUL NO LED! #####");
-    Serial.println("");
-    matrizRGB[linha][3] = matrizRGB[linha][3] + 1;
+    matrizRGB[linha][3]++;
+    analogWrite(pinos[0], matrizRGB[linha][0]);
+    analogWrite(pinos[1], matrizRGB[linha][1]);
+    analogWrite(pinos[2], matrizRGB[linha][2]);
   }
-  else if (linha == 4) {
-    Serial.println("##### COR AMARELO NO LED! #####");
-    Serial.println("");
-    matrizRGB[linha][3] = matrizRGB[linha][3] + 1;
+  else if (linha == 4){
+    Serial.println("##### COR AMARELA NO LED! #####");
+    matrizRGB[linha][3]++;
+    analogWrite(pinos[0], matrizRGB[linha][0]);
+    analogWrite(pinos[1], matrizRGB[linha][1]);
+    analogWrite(pinos[2], matrizRGB[linha][2]);
   }
-  else if (linha == 5) {
+  else if (linha == 5){
     Serial.println("##### COR MAGENTA NO LED! #####");
-    Serial.println("");
-    matrizRGB[linha][3] = matrizRGB[linha][3] + 1;
+    matrizRGB[linha][3]++;
+    analogWrite(pinos[0], matrizRGB[linha][0]);
+    analogWrite(pinos[1], matrizRGB[linha][1]);
+    analogWrite(pinos[2], matrizRGB[linha][2]);
   }
-  else if (linha == 6) {
+  else if (linha == 6){
     Serial.println("##### COR ROSA NO LED! #####");
-    Serial.println("");
-    matrizRGB[linha][3] = matrizRGB[linha][3] + 1;
-
+    matrizRGB[linha][3]++;
+    analogWrite(pinos[0], matrizRGB[linha][0]);
+    analogWrite(pinos[1], matrizRGB[linha][1]);
+    analogWrite(pinos[2], matrizRGB[linha][2]);
   }
-
 }
 
 void Matriz_mostra() {
@@ -65,58 +63,37 @@ void Matriz_mostra() {
   for (int i = 0; i < 6; i++) {
     total += matrizRGB[i][3];
   }
+
+  Serial.println("");
   Serial.println("##### QUANTIDADE DE VEZES ACESSADO CADA COR: #####");
   for (int i = 0; i < 6; i++) {
     if (i == 0) {
       Serial.print("COR VERMELHA: ");
       Serial.print(matrizRGB[i][3]);
       Serial.println(" vezes!");
-
     } else if (i == 1) {
       Serial.print("COR VERDE: ");
       Serial.print(matrizRGB[i][3]);
       Serial.println(" vezes!");
-      float percentual = (total == 0) ? 0 : (matrizRGB[i][3] * 100.0 / total);
-      Serial.print(percentual, 2);
-      Serial.println("%");
-
     } else if (i == 2) {
       Serial.print("COR AZUL: ");
       Serial.print(matrizRGB[i][3]);
       Serial.println(" vezes!");
-      float percentual = (total == 0) ? 0 : (matrizRGB[i][3] * 100.0 / total);
-      Serial.print(percentual, 2);
-      Serial.println("%");
-
     } else if (i == 3) {
       Serial.print("COR AMARELA: ");
       Serial.print(matrizRGB[i][3]);
       Serial.println(" vezes!");
-      float percentual = (total == 0) ? 0 : (matrizRGB[i][3] * 100.0 / total);
-      Serial.print(percentual, 2);
-      Serial.println("%");
-
     } else if (i == 4) {
       Serial.print("COR MAGENTA: ");
       Serial.print(matrizRGB[i][3]);
       Serial.println(" vezes!");
-      float percentual = (total == 0) ? 0 : (matrizRGB[i][3] * 100.0 / total);
-      Serial.print(percentual, 2);
-      Serial.println("%");
-
     } else if (i == 5) {
       Serial.print("COR ROSA: ");
       Serial.print(matrizRGB[i][3]);
       Serial.println(" vezes!");
-      float percentual = (total == 0) ? 0 : (matrizRGB[i][3] * 100.0 / total);
-      Serial.print(percentual, 2);
-      Serial.println("%");
     }
   }
-
 }
-
-
 
 void setup() {
   for (int i = 0; i < 3; i++) { // só 3 pinos
@@ -125,7 +102,6 @@ void setup() {
   pinMode(PushButtom1, INPUT);
   pinMode(pushMostraMatriz, INPUT);
   Serial.begin(9600);
-
 }
 
 void loop() { // CHAMADOR DE FUNÇÕES
@@ -137,5 +113,4 @@ void loop() { // CHAMADOR DE FUNÇÕES
     Matriz_mostra();
     delay(500);
   }
-
 }
