d979a56b182401e75004ad6a38695a3f8e7d96eb
_Malagutt1_ <kauamarcosmalagutti2003@gmail.com>
2025-09-12 16:02:55 -0300

    Commit orgabnizacao _ beckup

---
+++ BECKUP_SERVIDOR/listamac.php
@@ -0,0 +1,45 @@
+Array
+(
+    [idmac] => 48:E7:29:55:38:89
+    [0] => 48:E7:29:55:38:89
+    [nome] => ESP Vermelho
+    [1] => ESP Vermelho
+    [contador] => 8
+    [2] => 8
+)
+Array
+(
+    [idmac] => 84:0D:8E:B0:82:5C
+    [0] => 84:0D:8E:B0:82:5C
+    [nome] => ESP Azul
+    [1] => ESP Azul
+    [contador] => 0
+    [2] => 0
+)
+Array
+(
+    [idmac] => 94:B9:7E:15:FB:FD
+    [0] => 94:B9:7E:15:FB:FD
+    [nome] => Esp8266 BancoWeb
+    [1] => Esp8266 BancoWeb
+    [contador] => 0
+    [2] => 0
+)
+Array
+(
+    [idmac] => CC:50:E3:05:19:BA
+    [0] => CC:50:E3:05:19:BA
+    [nome] => ESP Branco
+    [1] => ESP Branco
+    [contador] => 12
+    [2] => 12
+)
+Array
+(
+    [idmac] => CC:50:E3:3C:1E:52
+    [0] => CC:50:E3:3C:1E:52
+    [nome] => ESP Verde
+    [1] => ESP Verde
+    [contador] => 9
+    [2] => 9
+)
